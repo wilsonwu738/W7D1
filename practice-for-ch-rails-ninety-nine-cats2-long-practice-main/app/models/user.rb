5feb090be6727923f15c3a94e37d349c54e0e13b
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2022-12-19 16:03:14 -0500

    did the thing

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
+  validates :password, length: {minimum: 6}, allow_nil: true
   before_validation :ensure_session_token
 
+  attr_reader :password
   def password=(new_pass)
-    password_digest = BCrypt::Password.create(new_pass)
+    self.password_digest = BCrypt::Password.create(new_pass)
     @password = new_pass
   end
 
@@ -14,11 +16,26 @@ class User < ApplicationRecord
   end
 
   def ensure_session_token
-    session_token ||= SecureRandom::urlsafe_base64
+    self.session_token ||= generate_unique_session_token
   end
 
+  def reset_session_token!
+    self.session_token = generate_unique_sesion_token
+    self.save!
+    self.session_token
+  end
 
+  def self.find_by_credential(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
 
-
-
+  private
+  def generate_unique_session_token
+    SecureRandom::urlsafe_base64
+  end
 end
--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/config/routes.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users
+  resources :users, only: [:new, :create]
 end
 
